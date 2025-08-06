2c6febdc6026a5b4a0dd0d8a903cf0dcb8ff5d1c
Hasono <2105689721@qq.com>
2025-08-06 19:56:23 +0800

    chore: 新增antd对于react 19补丁依赖

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./assets/css/index.css";
 import { RouterProvider } from "react-router";
 import router from "./router";
-// import "@ant-design/v5-patch-for-react-19";
+import "@ant-design/v5-patch-for-react-19";
 
 createRoot(document.getElementById("root")!).render(
   <RouterProvider router={router} />
