1850223ef4fbdefe5db2f5e7a805e8a8c5ec8b06
Hasono <2105689721@qq.com>
2025-08-17 22:40:25 +0800

    feat: 完成AuthTransition组件

--- src/components/Auth/AuthTransition/AuthTransition.tsx
+++ src/components/Auth/AuthTransition/AuthTransition.tsx
@@ -1,7 +1,29 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
+import { SwitchTransition, CSSTransition } from "react-transition-group";
+import { useRef } from "react";
+import "./AuthTransition.css";
 
 const AuthTransition = () => {
-  return <Outlet />;
+  const location = useLocation();
+  const nodeRef = useRef(null);
+
+  return (
+    <div className="auth-transition-container">
+      <SwitchTransition mode="out-in">
+        <CSSTransition
+          key={location.pathname}
+          nodeRef={nodeRef}
+          timeout={250}
+          classNames="auth-slide"
+          unmountOnExit
+        >
+          <div ref={nodeRef} className="auth-transition-wrapper">
+            <Outlet />
+          </div>
+        </CSSTransition>
+      </SwitchTransition>
+    </div>
+  );
 };
 
 export default AuthTransition;
